04bb581b04935a662569ddcd79e7fa72cf1b8c7e
James Davies <james.davies@kineticdata.com>
2023-12-05 20:54:33 -0500

    Adding handler for creating aws buckets

---
+++ handlers/amazon_s3_bucket_create_v1/handler/vendor/aws-sdk-core-2.2.34/lib/aws-sdk-core/version.rb
@@ -0,0 +1,3 @@
+module Aws
+  VERSION = '2.2.34'
+end
---
+++ handlers/amazon_s3_bucket_create_v1/test/simple_input.rb
@@ -0,0 +1,19 @@
+{
+  'info' => {
+    'access_key' => '',
+    'secret_key' => '',
+    'region' => 'us-east-1',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'bucket' => 'james-test-octo-bucket',
+    'region' => '',
+    'acl'    => '',
+    'grant_full_control' => '',
+    'grant_read' => '',
+    'grant_read_acp' => '',
+    'grant_write' => '',
+    'grant_write_acp' => '',
+    'object_lock_enabled_for_bucket' => ''
+  }
+}
